32dbffcfe43d44a040ef3c51bbb2f9ee0c31d625
Chris Anderson <jchris@apache.org>
2011-08-05 14:19:34 -0700

    import friendly helper

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -1,5 +1,22 @@
 // Apache 2.0 J Chris Anderson 2011
 $(function() {   
+    // friendly helper http://tinyurl.com/6aow6yn
+    $.fn.serializeObject = function() {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+
     var path = unescape(document.location.pathname).split('/'),
         design = path[3],
         db = $.couch.db(path[1]);
@@ -31,10 +48,11 @@ $(function() {
         loggedIn : function(r) {
             $("#profile").couchProfile(r, {
                 profileReady : function(profile) {
-                    $("#create-message").submit(function(){
-                        var form = this, doc = {};
-                            doc.created_at = new Date();
-                            doc.profile = profile;
+                    $("#create-message").submit(function(e){
+                        e.preventDefault();
+                        var form = this, doc = $(form).serializeObject();
+                        doc.created_at = new Date();
+                        doc.profile = profile;
                         db.saveDoc(doc, {success : function() {form.reset();}});
                         return false;
                     }).find("input").focus();
